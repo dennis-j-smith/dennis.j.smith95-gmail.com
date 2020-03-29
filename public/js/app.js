9a684df7120377686856758f771a73dcb56534cc
Dennis Smith <dennis.j.smith95@gmail.com>
2020-03-29 15:58:54 -0400

    initial

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message1')
+const message2 = document.querySelector('#message2')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value
+  console.log(location)
+  message1.textContent = 'Loading...'
+  message2.textContent = ''
+  fetch('/weather?address=' + location).then((response) => {
+
+      response.json().then((data) => {
+        if (data.error) {
+          message1.textContent = data.error
+        }
+        else  {
+          message1.textContent = data.location
+          console.log(data.forecast)
+          message2.textContent = data.forecast.temp
+        }
+      })
+  })
+})
---
+++ src/app.js
@@ -0,0 +1,91 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static dir to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+      title: 'Weather App',
+      name: 'Dennis'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+      title: 'About Me',
+      name: 'Dennis'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+      title: 'Help Page',
+      helpmsg: 'My help message',
+      name: 'Dennis'
+    })
+})
+
+app.get('/weather', (req, res) => {
+
+    if (!req.query.address) {
+      return res.send({error:  'Please provide an address'})
+    }
+
+    geocode (req.query.address, (error, {latitude, longitude, location} = {}) => {
+
+        if (error) {
+            //shorthand
+            return res.send({error})
+            //return res.send({error:  error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+
+            if (error) {
+              return res.send({error:  error})
+            }
+
+            res.send({
+              address: req.query.address,
+              location: location,
+              forecast: forecastData
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('notfound', {
+      title: 'Not Found',
+      msg: 'Help Article not found.',
+      name: 'Dennis'
+    })
+})
+
+app.get('*', (req,res) => {
+  res.render('notfound', {
+    title: 'Not Found',
+    msg: 'Page not found.',
+    name: 'Dennis'
+  })
+})
+
+app.listen(port, () => {
+  console.log('Server is up on port ' + port)
+})
